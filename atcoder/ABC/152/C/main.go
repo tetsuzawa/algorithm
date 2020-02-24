d8212bbaaf8b770eed3100a40c189a5cc146b36e
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-02-24 11:25:56 +0900

    Add 152

---
+++ atcoder/ABC/152/C/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var nextReader func() string
+
+func newScanner() func() string {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Buffer(make([]byte, 1024), int(1e11))
+	sc.Split(bufio.ScanWords)
+	return func() string {
+		sc.Scan()
+		return sc.Text()
+	}
+}
+
+func nextInt() int { n, _ := strconv.Atoi(nextReader()); return n }
+
+func nextInts(size int) []int {
+	ns := make([]int, size)
+	for i := 0; i < size; i++ {
+		ns[i] = nextInt()
+	}
+	return ns
+}
+
+func main() {
+	nextReader = newScanner()
+	N := nextInt()
+	P := nextInts(N)
+
+	if P[0] != N && P[0] != 1 {
+		fmt.Println(P[0]-1)
+	} else {
+		fmt.Println(P[0])
+	}
+}
