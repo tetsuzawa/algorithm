045819b37bb773134a842cd73c83c87fe8b1a108
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-07-09 09:20:59 +0900

    Add mini max sum

---
+++ go/hckerrank/practice/algorithm/warmup/mini-mux_sum/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the miniMaxSum function below.
+func miniMaxSum(arr []int32) {
+	min := arr[0]
+	max := arr[0]
+	var sum int32
+	for _, v := range arr {
+		sum += v
+		if v > max {
+			max = v
+		} else if v < min {
+			min = v
+		}
+	}
+	fmt.Println(sum-max, sum-min)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	arrTemp := strings.Split(readLine(reader), " ")
+
+	var arr []int32
+
+	for i := 0; i < 5; i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
